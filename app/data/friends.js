33c578bfc3b215ebc708996c7ce9122178f085e2
jimmy <rucker>
2019-07-27 00:13:42 -0500

    orginal files

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friendsArray = [
+  {
+    name: "Jimmy",
+    photo: "https://placeimg.com/300/300/people",
+    scores: [4, 1, 3, 3, 5, 2, 5, 1, 1, 5]
+  },
+  {
+    name: "Ericka",
+    photo: "https://placeimg.com/300/300/people",
+    scores: [3, 4, 4, 1, 3, 3, 2, 2, 3, 4]
+  },
+  {
+    name: "Krystal",
+    photo: "https://placeimg.com/300/300/people",
+    scores: [5, 1, 5, 2, 3, 5, 2, 2, 4, 5]
+  },
+  {
+    name: "Dennis",
+    photo: "https://placeimg.com/300/300/people",
+    scores: [1, 1, 3, 4, 1, 3, 1, 1, 5, 1]
+  },
+  {
+    name: "Brandon",
+    photo: "https://placeimg.com/300/300/people",
+    scores: [5, 1, 3, 3, 1, 3, 1, 4, 1, 5]
+  }
+];
+
+module.exports = friendsArray;
